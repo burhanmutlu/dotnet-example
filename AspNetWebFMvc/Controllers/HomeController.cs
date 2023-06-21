95ee93f47aa8964c164b9c71f469208b2b9de4fc
DESKTOP-IAVPDO1\Burhan Mutlu <burhackmutlu@gmail.com>
2023-06-21 15:48:08 +0300

    örneklere başlandı

---
+++ AspNetWebFMvc/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft;
+
+namespace AspNetCoreWebAppMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+		public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
